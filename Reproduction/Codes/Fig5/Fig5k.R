d06e5630490bac113c463072af37a51be9c67b09
BIGHanLab <167292686+BIGHanLab@users.noreply.github.com>
2025-12-16 13:18:26 +0800

    Delete Reproduction/Codes directory

--- Reproduction/Codes/Fig5/Fig5k.R
+++
@@ -1,8 +0,0 @@
-setwd('/data/xy/Spatial_transcriptome/eWEIDE/Reproduction/Codes/Fig5/')
-
-TCGA_COAD_total_with_20q<-readRDS("../../Data/Fig4_5/TCGA_COAD_total_with_20q.RDS")
-TCGA_COAD_total_with_20q$chr20q_status.y<-factor(TCGA_COAD_total_with_20q$chr20q_status.y,levels=c("Normal","WT","Gain"))
-ggplot(TCGA_COAD_total_with_20q[!is.na(TCGA_COAD_total_with_20q$chr20q_status.y),],aes(x=chr20q_status.y,y=log2(HERVH_expression),color=chr20q_status.y))+
-  geom_boxplot(size=1)+scale_color_manual(values = c("#BFBDBD","#1C6797","#DD7E2D"))+labs(x="",y="log2(RPM)",title="HERV-H")+guides(fill=F,color=F)+
-  theme(plot.title = element_text(hjust=0.5))+geom_signif(comparisons = list(c("Normal", "WT"),c("Normal", "Gain"),c("WT", "Gain")),y_position=c(8, 9, 8))
-
